5486662f182cda9d87bf4212f71a9b25155578dd
Paul Bender <paulbender@live.com>
2021-06-27 16:58:30 -0400

    Bump to version v1.4

--- inc/plugins/pmlog.php
+++ inc/plugins/pmlog.php
@@ -30,7 +30,7 @@ function pmlog_info()
 		"website"			=> "http://galaxiesrealm.com/index.php",
 		"author"			=> "Starpaul20",
 		"authorsite"		=> "http://galaxiesrealm.com/index.php",
-		"version"			=> "1.3.1",
+		"version"			=> "1.4",
 		"codename"			=> "pmlog",
 		"compatibility"		=> "18*"
 	);
